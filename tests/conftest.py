43a30e2556fffd0fd4b349efe56a8287248ef92a
Guillaume Lesaine <guillaume.lesaine@protonmail.com>
2019-05-19 18:28:05 +0200

    0.31 - Tests engine update

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,51 +1,25 @@
 import pytest
 import json
 import pandas as pd
-import inspect
+import sys, os
 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-# @pytest.fixture
-# def csv_loader():
-#     """Loads data from csv file"""
-#
-#     def _loader(filename):
-#         df = pd.read_csv("./tests/csv/" + filename)
-#         return df
-#
-#     return _loader
-#
-# @pytest.fixture
-# def json_loader():
-#     """Loads data from json file"""
-#
-#     def _loader(filename,case,type = None):
-#         with open("./tests/json/" + filename, 'r') as f:
-#             dict = json.load(f)[case]
-#
-#             if type == "input" or type == "output":
-#                 data = dict[type]
-#             else :
-#                 data = tuple(dict.values())
-#
-#         return data
-#
-#     return _loader
+import package
 
-# def discover_function(z):
-#     function = inspect.getframeinfo(z).function.replace("test_","")
-#     method = getattr(functions, function)
-#
-#     return function, method
-#
-# @pytest.fixture
-# def discover_function(scope="module"):
-#     """Return the name of the function being tested and its method"""
-#
-#     def _discoverer(z):
-#
-#         function = inspect.getframeinfo(z).function.replace("test_","")
-#         method = getattr(functions, function)
-#
-#         return function, method
-#
-#     return _discoverer
+def load_json(filename, case=None):
+    with open("./tests/json/" + filename, 'r') as f:
+        if case:
+            dict = json.load(f)[case]
+            if len(dict) == 1:
+                data = dict[next(iter(dict))]
+            else :
+                data = tuple(dict.values())
+            return data
+        else:
+            dict = json.load(f)
+            return dict
+
+def load_csv(filename):
+    df = pd.read_csv("./tests/csv/" + filename)
+    return df
