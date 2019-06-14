cfd284dec02236e706a42d8e43c362d1cf6852da
Guillaume Lesaine <guillaume.lesaine@protonmail.com>
2019-06-14 20:34:49 +0200

    0.63 - sentence_counter.py

--- sentencescount.sh
+++ sentencescount.sh
@@ -7,7 +7,7 @@ rm -r outputs/sentences/*
 for entry in "./input"/*
 do
   filename=$(basename $entry .txt)
-  cat ./input/$filename.txt | ./package/shaper.py | ./package/sentencer.py | sort -k1,1 | ./package/reducer.py > ./outputs/sentences/$filename.csv
+  cat ./input/$filename.txt | ./package/shaper.py | ./package/sentence_counter.py | sort -k1,1 | ./package/reducer.py > ./outputs/sentences/$filename.csv
   echo $filename
 done
 
